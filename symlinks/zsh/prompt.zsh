ac67b6aa74fcf6725b68f09bf89bc63c8fee1bc6
Ayan Yenbekbay <ayan.yenb@gmail.com>
2016-10-24 18:37:38 +0600

    refactor: add missing hashbangs

--- symlinks/zsh/prompt.zsh
+++ symlinks/zsh/prompt.zsh
@@ -1,3 +1,5 @@
+#!/usr/bin/env zsh
+
 ZSH_THEME_GIT_PROMPT_PREFIX="%{$fg_bold[white]%}"
 ZSH_THEME_GIT_PROMPT_SUFFIX="%{$reset_color%}"
 ZSH_THEME_GIT_PROMPT_CLEAN="%{$fg_bold[green]%}✓%{$reset_color%}"
