274d56fc1995dfda5962c1d3c84572f404cc8dd3
Ayan Yenbekbay <ayan.yenb@gmail.com>
2017-01-07 16:42:52 +0600

    feat(install.sh): update pyenv and rbenv's installed versions

--- install.sh
+++ install.sh
@@ -48,8 +48,8 @@ echo ""
 if [[ $REPLY =~ ^[Yy]$ ]]; then
   # Configure python environment
   brew install pyenv
-  pyenv install 3.5.2
-  pyenv global 3.5.2
+  pyenv install 3.6.0
+  pyenv global 3.6.0
   pyenv rehash
 fi
 
@@ -58,8 +58,8 @@ echo ""
 if [[ $REPLY =~ ^[Yy]$ ]]; then
   # Configure ruby environment
   brew install rbenv ruby-build
-  rbenv install 2.3.1
-  rbenv global 2.3.1
+  rbenv install 2.4.0
+  rbenv global 2.4.0
   rbenv rehash
 fi
 
